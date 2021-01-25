0e8fd24624e8a4ee389f01243c98f1b15aed7d4f
ManuelS0usa <manelcssousa@gmail.com>
2021-01-25 17:48:08 +0000

    solving dash datatable bug

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
+import base64
+import datetime
+import io
+import plotly.graph_objs as go
+import cufflinks as cf
+
 import dash
+from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
+
+import pandas as pd
+
 
 # import some pre created and available styling
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 # initiate plotly app, with css style imported
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 # define some standard styling
 colors = {
@@ -36,6 +48,112 @@ app.layout = html.Div([
         },        
         multiple=True  # Allow multiple files to be uploaded
     ),
-    dcc.Graph(id='Mygraph'),
+    # dcc.Graph(id='Mygraph', figure={}),
     html.Div(id='output-data-upload')
 ])
+
+
+def parse_data(contents, filename):
+    """ parse targeted data file types """
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV or TXT file
+            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+        elif 'txt' or 'tsv' in filename:
+            # Assume that the user upl, delimiter = r'\s+'oaded an excel file
+            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')), delimiter = r'\s+')
+    except Exception as e:
+        print(e)
+        return html.Div(['There was an error processing this file.'])
+
+    return df
+
+
+#@app.callback(Output('Mygraph', 'figure'),
+#            [
+#                Input('upload-data', 'contents'),
+#                Input('upload-data', 'filename')
+#            ])
+
+
+def old_update_graph(contents, filename):
+    fig = {
+        'layout': go.Layout(
+            plot_bgcolor=colors["graphBackground"],
+            paper_bgcolor=colors["graphBackground"])
+    }
+
+    if contents:
+        contents = contents[0]
+        filename = filename[0]
+        df = parse_data(contents, filename)
+        df = df.set_index(df.columns[0])
+        print(df)
+        fig['data'] = df.iplot(asFigure=True, kind='scatter', mode='lines+markers', size=1)
+
+    return fig
+
+def update_graph(contents, filename):
+    if contents:
+        contents = contents[0]
+        filename = filename[0]
+        df = parse_data(contents, filename)
+        df = df.set_index(df.columns[0])
+        print("AQUI", df)
+
+        fig = go.Figure(
+            data=[go.Scatter(
+                mode='lines+markers',
+                size=1
+            )]
+        )
+
+        fig.update_layout(
+            plot_bgcolor=colors["graphBackground"],
+            paper_bgcolor=colors["graphBackground"]
+        )
+    return fig
+
+
+@app.callback(
+    Output('output-data-upload', 'children'),
+    [
+        Input('upload-data', 'contents'),
+        Input('upload-data', 'filename')
+    ]
+)
+
+
+def update_table(contents, filename):
+    table = html.Div()
+
+    if contents:
+        contents = contents[0]
+        filename = filename[0]
+        # df = parse_data(contents, filename)
+        df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
+
+        table = html.Div([
+            html.H5(filename),
+            dash_table.DataTable(
+                data=df.to_dict('records'),
+                columns=[{'name': i, 'id': i} for i in df.columns]
+            ),
+            html.Hr(),
+            html.Div('Raw Content'),
+            html.Pre(contents[0:200] + '...', style={
+                'whiteSpace': 'pre-wrap',
+                'wordBreak': 'break-all'
+            })
+        ])
+
+    return table
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
